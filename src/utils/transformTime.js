de2669dcb722e837d1cb3f0aebb453f8635eca38
Maria Kukharchuk <maria.kukharchuk@itrexgroup.com>
2020-10-20 13:19:40 +0300

    Fixed time

--- src/utils/transformTime.js
+++ src/utils/transformTime.js
@@ -1,6 +1,6 @@
 const transformTime = (ms) => {
   var minutes = Math.floor(ms / 60);
-  var seconds = ms.toFixed(0);
+  var seconds = ms.toFixed(0) - minutes * 60;
   return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 };
 
